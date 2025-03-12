9bc3c94a27e2b6890b27f2a4d774cb720ffbdd93
ducanhpham0312 <pdanh0312@gmail.com>
2025-03-12 18:02:39 +0200

    test multiple

--- script.py
+++ script.py
@@ -11,9 +11,11 @@ load_dotenv()
 
 # Configuration
 URL = 'https://www.tuni.fi/sportuni/omasivu/?page=selection&lang=en&type=3&area=2&week=0'
-SEARCH_TEXT = '09:00 Badminton'
+# SEARCH_TEXTS = ['19:00 Badminton', '20:00 Badminton', '21:00 Badminton', '20:30 Badminton', 
+#                 '21:30 Badminton', '17:00 Badminton', '18:00 Badminton']  
+SEARCH_TEXTS = ['07:00 Badminton', '08:00 Badminton', '09:00 Badminton']
 EMAIL_FROM = os.getenv('EMAIL_FROM')
-EMAIL_TO = os.getenv('EMAIL_TO')
+EMAIL_TO = os.getenv('EMAIL_TO').split(',')  # Assuming EMAIL_TO is a comma-separated list of emails
 SMTP_SERVER = os.getenv('SMTP_SERVER')
 SMTP_PORT = int(os.getenv('SMTP_PORT'))
 SMTP_USER = os.getenv('SMTP_USER')
@@ -23,20 +25,21 @@ def check_website():
     response = requests.get(URL)
     soup = BeautifulSoup(response.content, 'html.parser')
     
-    # Find elements containing the specific text
-    elements = soup.find_all(text=lambda text: text and SEARCH_TEXT in text)
+    found_elements = []
+    for search_text in SEARCH_TEXTS:
+        elements = soup.find_all(text=lambda text: text and search_text in text)
+        found_elements.extend(elements)
     
-    if elements:
-        notify(elements)
+    if found_elements:
+        notify(found_elements)
 
 def notify(elements):
-    subject = 'New HTML Element Found'
-    body = f'New HTML elements containing "{SEARCH_TEXT}" were found:\n\n'
+    subject = '[Auto] Lịch đánh cầu mới tìm thấy'
+    body = 'Vào SportUni đặt lịch đi nè, nhanh không kẻo lỡ:\n\n'
     body += '\n'.join([str(element) for element in elements])
     
     msg = MIMEMultipart()
     msg['From'] = EMAIL_FROM
-    msg['To'] = EMAIL_TO
     msg['Subject'] = subject
     msg.attach(MIMEText(body, 'plain'))
     
@@ -44,7 +47,11 @@ def notify(elements):
         server = smtplib.SMTP(SMTP_SERVER, SMTP_PORT)
         server.starttls()
         server.login(SMTP_USER, SMTP_PASS)
-        server.sendmail(EMAIL_FROM, EMAIL_TO, msg.as_string())
+        
+        for email in EMAIL_TO:
+            msg['To'] = email
+            server.sendmail(EMAIL_FROM, email, msg.as_string())
+        
         server.quit()
         print('Notification sent successfully.')
     except Exception as e:
